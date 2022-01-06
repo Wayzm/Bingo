c3ad50e1627290964bfd486cac9f8400883bc280
Wayzm <wyeumo@yahoo.fr>
2022-01-06 20:14:42 +0100

    ++

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <SDL2/SDL.h>
+
+
+
+
+
+
+//
+
+int main ()
+
+
+{
+
+
+
+
+//
+    SDL_CreateWindowAndRenderer(800, 800, SDL_WINDOW_OPENGL, &window, &renderer);
+}
