4a76bf9d1d30e1101200d306744116e105f89d3b
Wayzm <wyeumo@yahoo.fr>
2022-01-13 15:14:56 +0100

    inout ok

--- main.c
+++ main.c
@@ -6,6 +6,9 @@
 #include <SDL2/SDL.h>
 
 int a,b,c,d,e;
+char ans;
+int seed;
+int rnum;
 
 // On initialise la table de Bingo du joueur
 int *BingoTable;
@@ -15,9 +18,11 @@ int *BingoTable;
 int *CheckNumber;
 
 
-//Random integer generaotr between 1 and 100
-int randomintgenerator(){
- return 1;
+// Play function
+void play (int* BingoTable)
+{
+  rnum=rand()%100;
+
 }
 
 
@@ -25,25 +30,67 @@ int randomintgenerator(){
 
 int main ()
 
-
 {
-  printf(" Please input line numbers ! \n" );
-  printf(" Only Integers between 1 and 100 !\n" );
-  printf(" They need to be different! Dont try to scam me")
-  for (unsigned int i=0; i<4;i++)
+  BingoTable=(int*)malloc(sizeof(int)*4*5);
+  CheckNumber=(int*)malloc(sizeof(int)*100);
+
+  printf(" --- Please input line numbers ! --- \n" );
+  printf(" --- Only Integers between 1 and 100 ! --- \n" );
+  printf(" --- They need to be different! Dont try to scam me ! --- \n");
+  printf(" --- 4 Lines needed with 5 values for this Bingo Game --- \n \n");
+
+  printf("--- Do you wish to input a specific seed to play with others? [y/n] --- \n");
+  scanf("%c",&ans);
+
+  if(ans=='y')
   {
-    BingoTable=(int*)malloc(sizeof(int)*4*5);
-    CheckNumber=(int*)malloc(sizeof(int)*100);
-
-    scanf("%d %d %d %d %d", &a,&b,&c,&d,&e);
-
-      if(!a || !b || !c || !d || !e)
-        printf("Failed to get numbers for line number %d", i);
-
-    BingoTable[i]=(a,b,c,d,e);
+    printf("Please input your seed : \n");
+    scanf("%d",&seed);
+    srand(seed);
+  }
+  else
+  {
+    srand(time(NULL));
   }
 
+  printf("Let's make your Bingo sheet now !\n");
+
+  for(unsigned int i=0; i<20;i+=5)
+  {
+
+    printf("Input 5 different number for line number : %d \n \n", i%5+1);
+    scanf("%d %d %d %d %d", &a,&b,&c,&d,&e);
+    printf("\n");
+
+      if(!a || !b || !c || !d || !e)
+      {
+        printf("Failed to get numbers for line number %d \n", i%5 +1);
+        break;
+      }
+
+    BingoTable[i+0]=a;
+    BingoTable[i+1]=b;
+    BingoTable[i+2]=c;
+    BingoTable[i+3]=d;
+    BingoTable[i+4]=e;
+  }
+
+  printf("Voici notre matrice : \n");
+  for(unsigned int i=0; i<4;i++)
+  {
+    for(unsigned int j=0;j<5;j++)
+    {
+      printf("%d ", BingoTable[5*i+j]);
+    }
+    printf("\n");
+  }
+
+  printf("%d\n",rand()%101);
   return 0;
+
+  free(BingoTable);
+  free(CheckNumber);
+
 
 }
 
